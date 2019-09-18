c54c288682dd1b2981b35579d89a2fca03f2f2fa
gourav139 <55490573+gourav139@users.noreply.github.com>
2019-09-18 17:07:06 +0530

    Update profile.js

--- data/profile.js
+++ data/profile.js
@@ -1,50 +1,58 @@
 const profileData = {
     "title": "Profile",
-    "name": "Vipin Yadav",
-    "sub_title": "Full Stack Developer",
+    "name": "Gourav Sharma",
+    "sub_title": "DevOps Engineer",
     "about": {
         "intro": `I am a innovative and passionate developer with a primary interest in <strong>JavaScript</strong>.
         Experienced in designing and implementing sophisticated UI/UX with Progressive Web Application standards and application life-cycle management.`,
         "contact": {
-            "email": "imvpn22@gmail.com",
-            "phone": "+91-9919716378",
+            "email": "sharma.gauravgs123@gmail.com",
+            "phone": "+91-8283059293",
             "address": "Electronic City, Bengaluru, Karnataka",
-            "portfolio": "https://github.com/imvpn22"
+            "portfolio": "https://github.com/gourav139"
         }
     },
+    
     "links": {
-        "GitHub": "https://github.com/imvpn22",
-        "LinkedIn": "https://www.linkedin.com/in/imvpn22",
-        "Twitter": "https://twitter.com/imvpn22"
+        "GitHub": "https://github.com/gourav139",
+        "LinkedIn": "https://www.linkedin.com/in/gouravsharma7",
+        "Twitter": "https://twitter.com/"
     },
+    
     "education": [{
-            "alma": "FET, GKV, Haridwar",
-            "duration": "2014 - 2018",
-            "std": "Bachelor of Technology (Computer Science & Engineering)",
-            "score": "71%"
+            "alma": "DAV College, Amritsar",
+            "duration": "2016 - 2019",
+            "std": "Bachelor of Computer Applications",
+            "score": "66%"
         },
         {
-            "alma": "Jwala Devi SVMIC, Allahabad",
-            "duration": "2011 - 2013",
-            "std": "Class XI - XII (PCM)",
-            "score": "90%"
+            "alma": "DAV College, Amritsar",
+            "duration": "2014 - 2016",
+            "std": "Class XI - XII (Non-Med)",
+            "score": "74%"
+        },
+        {
+            "alma": "Anupam Montessorie School, Amritsar",
+            "duration": "2012 - 2014",
+            "std": "Class IX - X ",
+            "score": "79%"
         }
     ],
+    
     "skills": [{
             "graph": "false",
             "category": "Programming Languages",
             "topics": [
                 "C",
                 "C++",
-                "JavaScript"
+                "Java"
             ]
         },
         {
             "graph": "false",
             "category": "Databases",
             "topics": [
-                "MongoDB",
-                "PostgreSQL",
+                "MySQL",
                 "Oracle PL/SQL"
             ]
         },
@@ -52,75 +60,35 @@ const profileData = {
             "graph": "false",
             "category": "Frameworks / Libraries",
             "topics": [
-                "NodeJS",
                 "Angular",
-                "ReactJS",
-                "SCSS",
-                "LESS",
-                "Gulp",
-                "Grunt",
-                "Webpack"
+                "SCSS"
             ]
         }
     ],
+    
     "projects": [{
-            "title": "Restaurant Review",
+            "title": "Lorem Ipsum",
             "duration": "June - Nov 2018",
-            "link": "https://github.com/imvpn22/restaurant-review-pwa",
-            "desc": "Udacity's 'Mobile Web Specialist Restaurant Reviews App' Project for  MWS Nanodegree"
-        },
-        {
-            "title": "WhiteBoard",
-            "duration": "Jul - Aug 2017",
-            "link": "https://github.com/imvpn22/whiteboard",
-            "desc": "A simple messaging and sketch streaming web app for brainstorming sessions"
-        },
-        {
-            "title": "Jnanagni 2017",
-            "duration": "Jan - Mar 2017",
-            "link": "https://github.com/methusael13/jnanagni17",
-            "desc": "Official website for Jnanagni 2017 - FET, GKV's Annual Techfest"
+            "link": "https://github.com/gourav139",
+            "desc": "Lorem ipsum"
         }
     ],
+    
     "experiences": [{
             "desc": "<b>Project Engineer</b> at  <a target='_blank' href='https://www.wipro.com/'> <strong> Wipro </strong> </a>",
-            "date": "Aug 2018 - Present",
+            "date": "Jun 2019 - Present",
             "details": [
-                "Worked on various Web-Application projects, with couple of web technologies.",
-                "Currently working on a project with a tech-stack of Angular, Angular-Material and NodeJS-Express.",
-                "Responsible for implementing reusable UI/UX components and interacting with backend service.",
-            ]
-        },
-        {
-            "desc": "<b>Full-Stack Developer intern</b> at  <a target='_blank' href='https://www.venuemonk.com/'> <strong> Venuemonk </strong> </a> (<a target='_blank' href='https://drive.google.com/open?id=1ch_9x9jCp_TJwKY43FG-pCr_zV9H2iun'>Certificate: https://goo.gl/Uz1gaf</a>)",
-            "date": "Jan - Apr 2018",
-            "details": [
-                "Responsible for developing a web-application <b>Venue OnBoarding</b> from design to implementation.",
-                "Work included developing ReactJS-Redux frontend and integrating it with NodeJS-ExpressJS-MongoDB backend."
-            ]
-        },
-        {
-            "desc": "<b>Product development intern</b> at <a target='_blank' href='https://hasura.io/'> <strong> Hasura </strong> </a> (<a target='_blank' href='https://drive.google.com/file/d/0B6uOcoHYLgJ7UmlrUDlsMkVwajg/view?usp=sharing'>Certificate: https://goo.gl/8V3jBy</a>)",
-            "date": "May - Aug 2017",
-            "details": [
-                "Responsible for developing web-application using Hasura platform APIs.",
-                "Work included designing and implementing frontend application in HTML5-CSS3-JavaScript."
+                "Currently working on a project as Automation Tester.",
+                "Responsible for creating Maual Testcases and Automation Scripts in Java and Selenium .",
             ]
         }
     ],
+    
     "events": [{
             "title": "certifications",
             "body": [{
                     "desc": "<strong>Mobile Web Specialist</strong>, a Nanodegree provided by <a target='_blank' href='https://in.udacity.com//'> <b>Udacity</b>  (<a target='_blank' href='https://confirm.udacity.com/RLMHXAWZ'>Certificate: https://goo.gl/RGRzVn</a>)",
                     "date": "May - Nov 2018"
-                },
-                {
-                    "desc": "<a target='_blank' href='https://www.imad.tech'><strong>IMAD</strong></a>, a MOOC provided by <a target='_blank' href='https://onlinecourses.nptel.ac.in/'> <b>NPTEL</b> </a> and <a target='_blank' href='https://hasura.io/'> <strong> Hasura</strong> </a> (<a target='_blank' href='http://nptel.ac.in/noc/E_Certificate/linkedin/noc17-cs06/NPTEL17CS0626270067AN.jpg'>Certificate: https://goo.gl/X3HEdR</a>)",
-                    "date": "Jan - Mar 2017"
-                },
-                {
-                    "desc": "<strong>ETCS</strong>, a Summer Training provided by <a href='http://www.mnnit.ac.in/' target='_blank'> <b>MNNIT </b> Allahabad</a> (<a target='_blank' href='https://drive.google.com/file/d/0B6uOcoHYLgJ7WEVmdlJDcWVZcEE/view?usp=sharing'>Certificate link</a>)",
-                    "date": "Jun - July 2016"
                 }
             ]
         },
@@ -130,14 +98,6 @@ const profileData = {
                     "desc": "<strong>NPAT</strong>(<b>N</b>ATIONAL <b>P</b>ROGRAMMING <b>A</b>PTITUDE <b>T</b>EST) <a target='_blank' href='https://npat.nptel.ac.in/npat_2017/certificate?id=117913621007613125255'>(Certificate link)</a> ",
                     "date": "Aug 2017"
                 },
-                {
-                    "desc": "Event Organizer in <strong>Jnanagni'17</strong> (Annual Techno-Cultural fest of FET, GKV)",
-                    "date": "Mar 2017"
-                },
-                {
-                    "desc": "Conducted a workshop for 1<sup>st</sup> and 2<sup>nd</sup> year students on <strong>Linux Essentials</strong>",
-                    "date": "Oct 2016"
-                }
             ]
         }
     ]
